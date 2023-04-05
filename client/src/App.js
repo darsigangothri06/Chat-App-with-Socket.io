625ed4f0a2e0a98ee6445bce6811a41795af38fc
darsigangothri06 <darsigangothri@gmail.com>
2023-04-05 16:46:00 +0530

    Add: Frontend Setup for Sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
-  return (
-    <>
-      <h2>hello world</h2>
-    </>
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return(
+    <div className='App'>
+      {!loggedIn ? <Login/> : <h2>You are logged In</h2>}
+    </div>
   )
 }
 
---
+++ client/src/components/Login/index.js
@@ -0,0 +1,13 @@
+const Login = () => {
+    return (
+        <div className="logIn">
+            <div className="inputs">
+                <input type="text" placeholder="Name..."></input>
+                <input type="text" placeholder="Room..."></input>
+            </div>
+            <button>Enter Chat</button>
+        </div>
+    )
+}
+
+export default Login
