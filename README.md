309fbcdd26eaf08823813ccdef4bdb0177ce625e
darsigangothri06 <darsigangothri@gmail.com>
2023-04-05 18:29:47 +0530

    Add: Received data from React to Node

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ This is a full stack MERN chat app built using socket.io
 ### Chat Application using Socket.io
 
 * Using rooms to dividing the chats
+
+#### Room
+
+Room number is to detect which chat is this.
+
+For example if you chat with your mom, you will have a room number differently from your dad.
+
+This is the idea what a room is. It is to identify which chat we are participating in.
--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import io from "socket.io-client"
 import './App.css';
-import Login from './components/Login';
+
+let socket;
+const CONNECTION_PORT = 'localhost:8000/';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  
+  const [room, setRoom] = useState('')
+  const [userName, setUserName] = useState('')
 
+  useEffect(() => {
+    socket = io(CONNECTION_PORT)
+  }, [CONNECTION_PORT])
+
+  const connectToRoom = () => {
+    socket.emit('join_room', room)
+  }
   return(
     <div className='App'>
-      {!loggedIn ? <Login/> : <h2>You are logged In</h2>}
+      {!loggedIn ? 
+
+        <div className="logIn">
+          <div className="inputs">
+              <input type="text" placeholder="Name..." onChange={(e) => {setUserName(e.target.value)}}></input>
+              <input type="text" placeholder="Room..." onChange={(e) => {setRoom(e.target.value)}}></input>
+          </div>
+          <button onClick={connectToRoom}>Enter Chat</button>
+        </div>
+      
+      : <h2>You are logged In</h2>}
     </div>
   )
 }
